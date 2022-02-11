290ac9da555077f561c42d32694df5d381bd55da
Sean Hung <yunshanghong@gmail.com>
2022-02-11 17:46:18 +0800

    modify mongo connect

--- src/configs/mongoConnect.ts
+++ src/configs/mongoConnect.ts
@@ -10,16 +10,18 @@ const initMongo = () => {
             (err: any) => {
                 let dbStatus = ''
                 if (err) {
-                dbStatus = `*    Error connecting to DB: ${err}\n****************************\n`
+                    dbStatus = `*    Error connecting to DB: ${err}\n****************************\n`
+                    console.log(dbStatus)
+                }else{
+                    dbStatus = `*    DB Connection: OK\n****************************\n`
+                    // Prints initialization
+                    console.log('****************************')
+                    console.log('*    Starting Server')
+                    console.log(`*    Port: ${process.env.PORT || 3000}`)
+                    console.log(`*    NODE_ENV: ${process.env.NODE_ENV}`)
+                    console.log(`*    Database: MongoDB`)
+                    console.log(dbStatus)
                 }
-                dbStatus = `*    DB Connection: OK\n****************************\n`
-                // Prints initialization
-                console.log('****************************')
-                console.log('*    Starting Server')
-                console.log(`*    Port: ${process.env.PORT || 3000}`)
-                console.log(`*    NODE_ENV: ${process.env.NODE_ENV}`)
-                console.log(`*    Database: MongoDB`)
-                console.log(dbStatus)
             }
         )
     }
