5b374930bb40c60e2cab5b44692173bc261dada5
Sean Hung <yunshanghong@gmail.com>
2022-03-26 13:42:03 +0800

    modify front header

--- frontend/src/app/layouts/header/header.component.ts
+++ frontend/src/app/layouts/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { RegisterTypeDialog } from 'src/app/dialogs/registerType/registerType.dialog';
+import { User, UserService } from 'src/app/services/user.service';
 
 @Component({
     selector: 'app-header',
@@ -9,13 +10,21 @@ import { RegisterTypeDialog } from 'src/app/dialogs/registerType/registerType.di
 })
 export class HeaderComponent implements OnInit{
 
+	user: User = null;
+
     constructor(
         private dialog: MatDialog,
 	    private router: Router,
+        private userService: UserService,
     ) {}
 
 	ngOnInit(){
-
+		this.userService.userChange.subscribe((user: User) =>{
+			this.user = user;
+			if(!user || !user.accessToken){
+				this.router.navigate([""]);
+			}
+		})
 	}
 
     onShowTypeDialog(){
@@ -25,4 +34,9 @@ export class HeaderComponent implements OnInit{
             type && this.router.navigate(['register', type]);
         });
     }
+
+    onLogout(){
+		this.userService.currentUser = null;
+		this.router.navigate(["/"]);
+	}
 }
