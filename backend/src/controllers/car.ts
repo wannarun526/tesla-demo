3dad9fe9e5ef8828425a5701b8b7b69c62ffdeac
Sean Hung <yunshanghong@gmail.com>
2022-04-12 21:46:06 +0800

    fix car mapping broken

--- backend/src/controllers/car.ts
+++ backend/src/controllers/car.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 import { CarModel } from '../schemas/car';
 import { BaseController } from './base';
 import { CarCreateReq, CarCreateResp, CarListResp } from '../models/car';
-import { FileModel } from '../schemas/file';
 
 class CarController extends BaseController {
 
@@ -25,7 +24,18 @@ class CarController extends BaseController {
                 insuranceCompany: body.insuranceCompany,
                 insuranceType: body.insuranceType,
                 sumAssured: body.sumAssured,
-                status: "pending"
+                status: "pending",
+                vl01: null,
+                vl02: null,
+                car01: null,
+                car02: null,
+                car03: null,
+                car04: null,
+                car05: null,
+                car06: null,
+                car07: null,
+                car08: null,
+                car09: null,
             }).save();
 
             return this.util.handleSuccess<CarCreateResp>(resp, { carId: newCar._id.toString() });
@@ -50,8 +60,8 @@ class CarController extends BaseController {
                 .populate({ path: "car06", select: "path" })
                 .populate({ path: "car07", select: "path" })
                 .populate({ path: "car08", select: "path" })
-                .populate({ path: "car09", select: "path" });
-
+                .populate({ path: "car09", select: "path" })
+                
             const result: Array<CarListResp> = cars.map(car => ({
                     model: car.model,
                     chargeType: car.chargeType,
@@ -67,15 +77,15 @@ class CarController extends BaseController {
                     sumAssured: car.sumAssured,
                     vl01: { docPath: car.vl01.path, base64: null},
                     vl02: { docPath: car.vl02.path, base64: null},
-                    car01: { docPath: car.car01.path, base64: null},
-                    car02: { docPath: car.car02.path, base64: null},
-                    car03: { docPath: car.car03.path, base64: null},
-                    car04: { docPath: car.car04.path, base64: null},
-                    car05: { docPath: car.car05.path, base64: null},
-                    car06: { docPath: car.car06.path, base64: null},
-                    car07: { docPath: car.car07.path, base64: null},
-                    car08: { docPath: car.car08.path, base64: null},
-                    car09: { docPath: car.car09.path, base64: null},
+                    car01: { docPath: car.car01?.path, base64: null},
+                    car02: { docPath: car.car02?.path, base64: null},
+                    car03: { docPath: car.car03?.path, base64: null},
+                    car04: { docPath: car.car04?.path, base64: null},
+                    car05: { docPath: car.car05?.path, base64: null},
+                    car06: { docPath: car.car06?.path, base64: null},
+                    car07: { docPath: car.car07?.path, base64: null},
+                    car08: { docPath: car.car08?.path, base64: null},
+                    car09: { docPath: car.car09?.path, base64: null},
                     status: car.status,
                 })
             );
