78e76baf0510b362b18947db578abe3fa1c7b1c1
Sean Hung <yunshanghong@gmail.com>
2022-06-11 15:52:26 +0800

    prettier

--- frontend/src/app/main/addTesla/addTesla.component.ts
+++ frontend/src/app/main/addTesla/addTesla.component.ts
@@ -16,66 +16,70 @@ import { UtilService } from 'src/app/services/util.service';
     selector: 'app-addTesla',
     templateUrl: './addTesla.component.html',
 })
-export class AddTeslaComponent implements OnInit{
-
-    step: number = 0;
+export class AddTeslaComponent implements OnInit {
+    step: number = 2;
     checkedContract: boolean = false;
     carInfoForm: FormGroup;
     insuranceForm: FormGroup;
-    base64 = {};
     
+    base64 = {};
+
     constructor(
         private dialog: MatDialog,
         private utilService: UtilService,
         private apiService: ApiService,
-        private router: Router,
+        private router: Router
     ) {}
 
-	ngOnInit(){
+    ngOnInit() {
         this.carInfoForm = new FormGroup({
-            "model": new FormControl(null, [Validators.required]),
-            "chargeType": new FormControl(null, [Validators.required]),
-            "spec": new FormControl(null, [Validators.required]),
-            "year": new FormControl(null, [Validators.required]),
-            "season": new FormControl(null, [Validators.required]),
-            "carNumber": new FormControl(null, [Validators.required]),
-            "vl01": new FormControl(null, [Validators.required]),
-            "vl02": new FormControl(null, [Validators.required]),
-            "car01": new FormControl(null, [Validators.required]),
-            "car02": new FormControl(null, [Validators.required]),
-            "car03": new FormControl(null, [Validators.required]),
-            "car04": new FormControl(null, [Validators.required]),
-            "car05": new FormControl(null, [Validators.required]),
-            "car06": new FormControl(null, [Validators.required]),
-            "car07": new FormControl(null, [Validators.required]),
-            "car08": new FormControl(null, [Validators.required]),
-            "car09": new FormControl(null, [Validators.required]),
-        })
+            model: new FormControl(null, [Validators.required]),
+            chargeType: new FormControl(null, [Validators.required]),
+            spec: new FormControl(null, [Validators.required]),
+            year: new FormControl(null, [Validators.required]),
+            season: new FormControl(null, [Validators.required]),
+            carNumber: new FormControl(null, [Validators.required]),
+            vl01: new FormControl(null, [Validators.required]),
+            vl02: new FormControl(null, [Validators.required]),
+            car01: new FormControl(null, [Validators.required]),
+            car02: new FormControl(null, [Validators.required]),
+            car03: new FormControl(null, [Validators.required]),
+            car04: new FormControl(null, [Validators.required]),
+            car05: new FormControl(null, [Validators.required]),
+            car06: new FormControl(null, [Validators.required]),
+            car07: new FormControl(null, [Validators.required]),
+            car08: new FormControl(null, [Validators.required]),
+            car09: new FormControl(null, [Validators.required]),
+        });
 
         this.insuranceForm = new FormGroup({
-            "insuranceStartDate": new FormControl(null, [Validators.required]),
-            "insuranceEndDate": new FormControl(null, [Validators.required]),
-            "replaceValue": new FormControl(null, [Validators.required]),
-            "insuranceCompany": new FormControl(null, [Validators.required]),
-            "insuranceType": new FormControl(null, [Validators.required]),
-            "sumAssured": new FormControl(null, [Validators.required]),
-        })
-	}
-
-    onSubmitStep0(){
-        this.checkedContract && (this.step ++);
+            insuranceStartDate: new FormControl(null, [Validators.required]),
+            insuranceEndDate: new FormControl(null, [Validators.required]),
+            replaceValue: new FormControl(null, [Validators.required]),
+            insuranceCompany: new FormControl(null, [Validators.required]),
+            insuranceType: new FormControl(null, [Validators.required]),
+            sumAssured: new FormControl(null, [Validators.required]),
+        });
     }
 
-    onSubmitStep1(){
-        this.carInfoForm.valid && (this.step ++);
+    onSubmitStep0() {
+        this.checkedContract && this.step++;
     }
 
-    onSubmitStep2(){
-        this.insuranceForm.valid && (this.step ++);
+    onSubmitStep1() {
+        this.carInfoForm.valid && this.step++;
     }
 
-    onSubmitStep3(){
-        if(this.checkedContract && this.carInfoForm.valid && this.insuranceForm.valid){
+    onSubmitStep2() {
+        this.insuranceForm.valid && this.step++;
+    }
+
+    onSubmitStep3() {
+        if (
+            this.checkedContract &&
+            this.carInfoForm.valid &&
+            this.insuranceForm.valid
+        ) {
             const req: CarCreateReq = {
                 model: this.carInfoForm.value.model,
                 chargeType: this.carInfoForm.value.chargeType,
@@ -85,78 +89,108 @@ export class AddTeslaComponent implements OnInit{
                 carNumber: this.carInfoForm.value.carNumber,
                 insuranceStartDate: this.insuranceForm.value.insuranceStartDate,
                 insuranceEndDate: this.insuranceForm.value.insuranceEndDate,
-                replaceValue: parseInt(this.insuranceForm.value.replaceValue, 10),
+                replaceValue: parseInt(
+                    this.insuranceForm.value.replaceValue,
+                    10
+                ),
                 insuranceCompany: this.insuranceForm.value.insuranceCompany,
                 insuranceType: this.insuranceForm.value.insuranceType,
                 sumAssured: parseInt(this.insuranceForm.value.sumAssured, 10),
-            }
-            this.apiService.CarCreate(req)
-            .pipe(
-                mergeMap((resp: CarCreateResp) =>{
-                    const allRequests: Promise<void>[] = []; 
-                    for(const key in this.carInfoForm.value){
-                        const item = this.carInfoForm.value[key];
-                        if(item instanceof File){
-                            const base64Split = this.base64[key]?.split("base64,");
-                            allRequests.push(
-                                this.apiService.FileCarUpload({
-                                    carId: resp.carId,
-                                    docName: (item as File).name,
-                                    docType: 
-                                        key === "vl01" || key ==="vl02" || key === "car01" || 
-                                        key === "car02" || key === "car03" || key === "car04" ||
-                                        key === "car05" || key === "car06" || key === "car07" ||
-                                        key === "car08" || key === "car09" ? key : "vl01",
-                                    docContent: base64Split[1],
-                                    mimeType: base64Split[0]
-                                }).toPromise()
-                            )
+            };
+            this.apiService
+                .CarCreate(req)
+                .pipe(
+                    mergeMap((resp: CarCreateResp) => {
+                        const allRequests: Promise<void>[] = [];
+                        for (const key in this.carInfoForm.value) {
+                            const item = this.carInfoForm.value[key];
+                            if (item instanceof File) {
+                                const base64Split =
+                                    this.base64[key]?.split('base64,');
+                                allRequests.push(
+                                    this.apiService
+                                        .FileCarUpload({
+                                            carId: resp.carId,
+                                            docName: (item as File).name,
+                                            docType:
+                                                key === 'vl01' ||
+                                                key === 'vl02' ||
+                                                key === 'car01' ||
+                                                key === 'car02' ||
+                                                key === 'car03' ||
+                                                key === 'car04' ||
+                                                key === 'car05' ||
+                                                key === 'car06' ||
+                                                key === 'car07' ||
+                                                key === 'car08' ||
+                                                key === 'car09'
+                                                    ? key
+                                                    : 'vl01',
+                                            docContent: base64Split[1],
+                                            mimeType: base64Split[0],
+                                        })
+                                        .toPromise()
+                                );
+                            }
                         }
+                        return combineLatest(allRequests);
+                    })
+                )
+                .subscribe(
+                    () => {
+                        this.router.navigate(['userInfo']);
+                    },
+                    (error: HttpErrorResponse) => {
+                        this.dialog.open(BasicInfoDialog, {
+                            data: {
+                                line1: error.error.errorMsg || error.message,
+                                line2: '請重新操作',
+                            },
+                        });
                     }
-                    return combineLatest(allRequests);
-                })
-            )
-            .subscribe(() =>{
-                this.router.navigate(["userInfo"])
-            }, 
-            (error: HttpErrorResponse) => {
-                this.dialog.open(BasicInfoDialog, {
-                    data: { line1: error.error.errorMsg || error.message, line2: "請重新操作" }
-                });
-            });
+                );
         }
-         
     }
 
-    onShowPicDemo(){
-        this.dialog.open(PicDemoDialog,{ 
+    onShowPicDemo() {
+        this.dialog.open(PicDemoDialog, {
             height: '90%',
         });
     }
 
-    onOpenUploadDocs(){
+    onOpenUploadDocs() {
         const uploadDocRef = this.dialog.open(UploadDocsDialog, {
-            width: "90%",
-            data: { vl01: this.carInfoForm.value.vl01, vl02: this.carInfoForm.value.vl02 },
+            width: '90%',
+            data: {
+                vl01: this.carInfoForm.value.vl01,
+                vl02: this.carInfoForm.value.vl02,
+            },
         });
 
-        uploadDocRef.afterClosed().subscribe(async result => {
-            if(result){
-                this.carInfoForm.patchValue({ vl01: result.vl01, vl02: result.vl02 });
-                this.base64["vl01"] = result.vl01 ? await this.utilService.onFileToBase64(result.vl01) : null;
-                this.base64["vl02"] = result.vl02 ? await this.utilService.onFileToBase64(result.vl02) : null;
+        uploadDocRef.afterClosed().subscribe(async (result) => {
+            if (result) {
+                this.carInfoForm.patchValue({
+                    vl01: result.vl01,
+                    vl02: result.vl02,
+                });
+                this.base64['vl01'] = result.vl01
+                    ? await this.utilService.onFileToBase64(result.vl01)
+                    : null;
+                this.base64['vl02'] = result.vl02
+                    ? await this.utilService.onFileToBase64(result.vl02)
+                    : null;
             }
         });
     }
 
-    async onUploadCarPic(field: string, file: File){
-        if(this.carInfoForm.get(field)){
+    async onUploadCarPic(field: string, file: File) {
+        if (this.carInfoForm.get(field)) {
             this.carInfoForm.get(field).setValue(file);
             this.base64[field] = await this.utilService.onFileToBase64(file);
         }
     }
 
-    onBackToPartner(){
-        this.router.navigate(["userInfo"]);
+    onBackToPartner() {
+        this.router.navigate(['userInfo']);
     }
 }
